9422284c8fcb525da5bde626435c236d66de7756
Daniel Hackney <dan@haxney.org>
2007-11-17 10:36:31 -0500

    Break out custom options into separate file.
    There is really no need to have them in the .emacs file, plus it was screwing up
    my symlink whenever it wrote changes.
    
    Signed-off-by: Daniel Hackney <dan@haxney.org>

---
+++ my-custom.el
@@ -0,0 +1,46 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(LaTeX-command "latex -shell-escape")
+ '(bongo-enabled-backends (quote (mplayer)))
+ '(case-fold-search t)
+ '(confirm-kill-emacs (quote yes-or-no-p))
+ '(current-language-environment "UTF-8")
+ '(default-input-method "rfc1345")
+ '(git-append-signed-off-by t)
+ '(jde-ant-enable-find t)
+ '(jde-ant-read-target t)
+ '(jde-build-function (quote (jde-ant-build)))
+ '(jde-complete-function (quote jde-complete-minibuf))
+ '(jde-complete-unique-method-names nil)
+ '(jde-enable-abbrev-mode t)
+ '(jde-jdk (quote ("1.5")))
+ '(jde-jdk-registry (quote (("1.5" . "/usr/lib/jvm/java-6-sun/"))))
+ '(load-home-init-file t t)
+ '(nxml-slash-auto-complete-flag t)
+ '(pgg-cache-passphrase nil)
+ '(pgg-default-user-id "A016D1D6")
+ '(planner-reverse-chronological-notes nil)
+ '(quack-default-program "mzscheme -g")
+ '(quack-global-menu-p nil)
+ '(quack-pretty-lambda-p nil)
+ '(quack-run-scheme-always-prompts-p nil)
+ '(quack-smart-open-paren-p nil)
+ '(show-paren-mode t nil (paren))
+ '(text-mode-hook (quote (turn-on-auto-fill text-mode-hook-identify)))
+ '(tramp-default-method "ssh")
+ '(transient-mark-mode t)
+ '(truncate-lines t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(diff-added ((t (:inherit diff-changed-face :background "Green"))))
+ '(diff-added-face ((t (:inherit diff-changed-face :background "Green"))) t)
+ '(diff-removed ((t (:inherit diff-changed-face :background "red"))))
+ '(diff-removed-face ((t (:inherit diff-changed-face :background "red"))) t)
+ '(flyspell-duplicate-face ((((class color)) (:foreground "Gold3" :underline t :weight bold))))
+ '(flyspell-incorrect-face ((((class color)) (:foreground "magenta" :underline t :weight bold)))))
