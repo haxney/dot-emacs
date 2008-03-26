29d6eb8c6b573e663204fccfe8333337577e2f60
Daniel Hackney <dan@haxney.org>
2008-03-25 22:54:35 -0400

    Set C-indentation style for CSS.
    Use 4 spaces for indent.
    
    Signed-off-by: Daniel Hackney <dan@haxney.org>

--- startup/css-init.el
+++ startup/css-init.el
@@ -1,3 +1,7 @@
 ;; CSS mode
 (autoload 'css-mode "css-mode" "Enter CSS-mode." t)
 (setq auto-mode-alist (cons '("\\.css$" . css-mode) auto-mode-alist))
+(eval-after-load "css-mode"
+  '(progn
+     (setq cssm-indent-function 'cssm-c-style-indenter)
+     (setq cssm-indent-level 4)))
