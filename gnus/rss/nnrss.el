5f2ff143b282c348e74dd2fc044e5fecfc84b634
Daniel Hackney <dan@haxney.org>
2008-05-11 02:21:15 -0400

    Add Gnus config.
    Signed-off-by: Daniel Hackney <dan@haxney.org>

---
+++ gnus/rss/nnrss.el
@@ -0,0 +1,3 @@
+;; -*- coding: utf-8-emacs; -*-
+(setq nnrss-group-alist '(("Journals.ars" "http://arstechnica.com/journals.rssx" "Ars Technica Journals") ("Ars Technica" "http://arstechnica.com/index.rssx" "The Art of Technology") ("Slashdot" "http://rss.slashdot.org/Slashdot/slashdot" "News for nerds, stuff that matters")))
+(setq nnrss-server-data '(("Slashdot" 60)))
