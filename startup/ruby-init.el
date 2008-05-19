1e38519baf4558d1527e0d9eb60d55525dcd9fd8
Daniel Hackney <dan@haxney.org>
2008-05-19 17:07:59 -0400

    Add 'Rakefile' to list of Ruby files.
    Signed-off-by: Daniel Hackney <dan@haxney.org>

--- startup/ruby-init.el
+++ startup/ruby-init.el
@@ -3,6 +3,7 @@
 
 ;; .rhtml loads html
 (add-to-list 'auto-mode-alist '("\\.rhtml$" . html-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 
 (add-hook 'ruby-mode-hook
           '(lambda ()
