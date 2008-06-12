4330702c6f1a69fb3cf95c340e3d8e08566cd5fa
Daniel Hackney <dan@haxney.org>
2008-06-12 02:29:55 -0400

    Use a width of 72 when visiting mail.google.com server buffers.
    Google wraps at 72 characters, so I might as well do so.
    
    Signed-off-by: Daniel Hackney <dan@haxney.org>

--- startup/server-init.el
+++ startup/server-init.el
@@ -1,6 +1,11 @@
-;; ---- Emacsclient
 ;; Only start when the server is not already started.
 (when (or (not (boundp 'server-process))
           (not (eq (process-status server-process)
                    'listen)))
   (server-start))
+
+;; When editing mail, set the goal-column to 72.
+(add-hook 'server-visit-hook
+          (lambda ()
+            (when (string-match "mail\.google\.com\.[0-9a-z]+\.txt" (buffer-name))
+              (set-fill-column 72))))
