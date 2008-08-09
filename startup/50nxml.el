e259a889afd42f98f94b816bb86debb44307254e
Daniel Hackney <dan@haxney.org>
2008-08-09 04:44:26 -0400

    Add `pretty-print-xml-region' to nXML.
    Also, work around `rng-schema-locating-files-default' being initially undefined.
    
    Signed-off-by: Daniel Hackney <dan@haxney.org>

--- startup/50nxml.el
+++ startup/50nxml.el
@@ -17,10 +17,28 @@
      (require 'flyspell)
      (add-to-list 'flyspell-prog-text-faces 'nxml-text-face)
 
+     (unless (boundp 'rng-schema-locating-files-default)
+       (setq rng-schema-locating-files-default '()))
+
      ;; Add new schemas to nXML
      (push (concat conf-home
                    (file-name-as-directory "schemas")
                    "schemas.xml")
-           rng-schema-locating-files-default)))
+           rng-schema-locating-files-default)
+     (defun pretty-print-xml-region (begin end)
+       "Pretty format XML markup in region. You need to have nxml-mode
+http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+this.  The function inserts linebreaks to separate tags that have
+nothing but whitespace between them.  It then indents the markup
+by using nxml's indentation rules."
+       (interactive "r")
+       (save-excursion
+         (nxml-mode)
+         (goto-char begin)
+         (while (search-forward-regexp "\>[ \\t]*\<" nil t)
+           (backward-char) (insert "\n"))
+         (indent-region begin end))
+       (message "Ah, much better!"))
+           ))
 
 ;;; 50nxml.el ends here
