d3bbcd518ad363a0e89085384acef0f896f11b34
Dan Hackney <dan@haxney.org>
2009-03-24 06:42:15 -0400

    Add erc config.
    Signed-off-by: Dan Hackney <dan@haxney.org>

---
+++ startup/50erc.el
@@ -0,0 +1,42 @@
+;;; 50erc.el --- Set ERC options.
+
+;; Copyright (C) 2008, Daniel Hackney
+
+;; Description: Set ERC options.
+;; Author: Daniel Hackney
+;; Maintainer: Daniel Hackney
+;; Keywords: irc local
+
+;; This file is NOT part of GNU Emacs.
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+(eval-after-load "erc"
+  '(progn
+     ;; Prevent these message types from triggering a display
+     (setq erc-track-exclude-types '("JOIN" "PART" "QUIT" "NICK" "MODE"))
+
+     ;; Use faces for styling
+     (setq erc-track-use-faces t)
+
+     (erc-timestamp-mode t)
+     (setq erc-timestamp-format "[%R-%m/%d]")
+
+     (setq erc-user-full-name "Daniel Hackney")
+     (setq erc-email-userid "dan@haxney.org")
+
+     (setq erc-max-buffer-size 20000)
+
+     (defun irc-maybe ()
+       "Connect to IRC."
+       (interactive)
+       (when (y-or-n-p "IRC? ")
+         (erc :server "irc.freenode.net" :port 6667
+              :nick "chrono325" :full-name "Daniel Hackney")
+         (erc :server "irc.oftc.net" :port 6667
+              :nick "chrono325" :full-name "Daniel Hackney")))))
+
+;;; 50erc.el ends here
