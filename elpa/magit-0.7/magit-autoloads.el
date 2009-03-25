5e6b01df148904d5533ed01e5919ebfea2a8d127
Dan Hackney <dan@haxney.org>
2009-03-25 01:56:21 -0400

    Add package magit-0.7
    It is a better git manager than the default.
    
    Signed-off-by: Dan Hackney <dan@haxney.org>

---
+++ elpa/magit-0.7/magit-autoloads.el
@@ -0,0 +1,27 @@
+;;; magit-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (magit-status) "magit" "magit.el" (18889 49902))
+;;; Generated autoloads from magit.el
+
+(autoload 'magit-status "magit" "\
+Not documented
+
+\(fn DIR)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("magit-pkg.el") (18889 49902 676014))
+
+;;;***
+
+(provide 'magit-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; magit-autoloads.el ends here
