af175630d28c85b6ba875952add8f94f3d424cd6
Dan Hackney <dan@haxney.org>
2009-03-25 02:22:06 -0400

    Re-enable byte-code-cache.el
    Change ordering of loaded packages to maintain correctness, such as custom.el
    needing to be loaded before byte-code-cache.
    
    Signed-off-by: Dan Hackney <dan@haxney.org>

---
+++ startup/15byte-code-cache.el
@@ -0,0 +1,20 @@
+;;; 01byte-code-cache.el --- Start up byte-code-cache.
+
+;; Copyright (C) 2008, Daniel Hackney
+
+;; Description: Start up byte-code-cache.
+;; Author: Daniel Hackney
+;; Maintainer: Daniel Hackney
+;; Keywords: elisp local
+
+;; This file is NOT part of GNU Emacs.
+
+;;; Commentary:
+;;
+;; This has been known to break some packages, so use with care.
+
+;;; Code:
+
+(require 'byte-code-cache)
+
+;;; 01byte-code-cache.el ends here
