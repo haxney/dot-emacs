8e6f33b89d3ee0808e9b0f4e2c443770fffe8778
Dan Hackney <dan@haxney.org>
2009-03-25 01:32:58 -0400

    Add loader for the nxhtml package.
    Signed-off-by: Dan Hackney <dan@haxney.org>

---
+++ startup/50nxhtml.el
@@ -0,0 +1,22 @@
+;;; 50nxhtml.el --- Load nXhtml.
+
+;; Copyright (C) 2008, Daniel Hackney
+
+;; Description: Load nXhtml.
+;; Author: Daniel Hackney
+;; Maintainer: Daniel Hackney
+;; Keywords: nxhtml local
+
+;; This file is NOT part of GNU Emacs.
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+(load (concat conf-home
+              (file-name-as-directory "elisp")
+              (file-name-as-directory "nxhtml")
+              "autostart.el"))
+
+;;; 50nxhtml.el ends here
