84a678ea998a856754edbe02afcacc57458bf421
Dan Hackney <dan@haxney.org>
2009-06-29 07:53:02 -0400

    Add different eproject.
    Figures out what the project should be from the filename and nearby files
    instead of a list. Prevents the annoying addition of files to projects when
    browsing random unrelated files.
    
    Signed-off-by: Dan Hackney <dan@haxney.org>

--- elpa/eproject-0.3/eproject-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; eproject-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "eproject" "eproject.el" (18981 39430))
-;;; Generated autoloads from eproject.el
-(require 'eproject)
-
-;;;***
-
-;;;### (autoloads nil nil ("eproject-config.el" "eproject-pkg.el")
-;;;;;;  (18981 39430 568627))
-
-;;;***
-
-(provide 'eproject-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; eproject-autoloads.el ends here
---
+++ startup/50eproject.el
@@ -0,0 +1,34 @@
+;;; 50eproject.el --- Define eproject types.
+
+;; Copyright (C) 2009 Daniel Hackney
+
+;; Author: Daniel Hackney
+;; Keywords: project elisp local
+
+;; This file is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation; either version 3, or (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful, but WITHOUT ANY
+;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+;; A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; GNU Emacs; see the file COPYING.  If not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+;; USA.
+
+;; This file is NOT part of GNU Emacs.
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+(require 'eproject)
+
+(define-project-type elisp (generic)
+  (and (look-for "*.el" :glob) (look-for ".eproject"))
+  :relevant-files ("\\.el$"))
+
+;;; 50eproject.el ends here
