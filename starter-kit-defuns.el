16098e39f6de2b62dd6b8c68236d91a200b2f152
Dan Hackney <dan@haxney.org>
2010-02-07 01:17:11 -0500

    Move `count-words-region' to "defuns" file.
    Signed-off-by: Dan Hackney <dan@haxney.org>

--- starter-kit-defuns.el
+++ starter-kit-defuns.el
@@ -359,6 +359,32 @@ by using nXML's indentation rules."
       (indent-region begin end)))
   (message "Ah, much better!"))
 
+(defun count-words-region (beginning end)
+  "Print number of words in the region."
+  (interactive "r")
+  (message "Counting words in region ... ")
+
+  ;; 1. Set up appropriate conditions.
+  (save-excursion
+    (let ((count 0))
+      (goto-char beginning)
+
+      ;; 2. Run the while loop.
+      (while (and (< (point) end)
+                  (re-search-forward "\\w+\\W*" end t))
+        (setq count (1+ count)))
+
+      ;; 3. Send a message to the user.
+      (cond ((zerop count)
+             (message
+              "The region does NOT have any words."))
+            ((= 1 count)
+             (message
+              "The region has 1 word."))
+            (t
+             (message
+              "The region has %d words." count))))))
+
 (provide 'starter-kit-defuns)
 
 ;;; starter-kit-defuns.el ends here
