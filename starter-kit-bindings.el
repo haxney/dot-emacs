f825fe25fb8a6ecfa36e9a3363742183a8708a2f
Dan Hackney <dan@haxney.org>
2010-12-25 15:53:11 -0500

    Don't use regex searches by default.
    Signed-off-by: Dan Hackney <dan@haxney.org>

--- starter-kit-bindings.el
+++ starter-kit-bindings.el
@@ -21,12 +21,6 @@
 (global-set-key (kbd "C-+") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
 
-;; Use regex searches by default.
-(global-set-key (kbd "C-s") 'isearch-forward-regexp)
-(global-set-key (kbd "C-r") 'isearch-backward-regexp)
-(global-set-key (kbd "C-M-s") 'isearch-forward)
-(global-set-key (kbd "C-M-r") 'isearch-backward)
-
 ;; Jump to a definition in the current file. (This is awesome.)
 (global-set-key (kbd "C-x C-i") 'ido-imenu)
 
