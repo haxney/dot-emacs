940613503ebfd2012ca6616ba7f034ab089b4a5f
Daniel Hackney <dan@haxney.org>
2012-10-25 00:16:51 -0400

    Don't error if `semantic-el' cannot be found.

--- load.d/50filetypes.el
+++ load.d/50filetypes.el
@@ -117,7 +117,7 @@
 
 (defun set-elisp-mode-name ()
   (setq mode-name "El"))
-(require 'semantic-el)
+(require 'semantic-el nil t)
 
 ;; Allow "/sudo:host:/etc/stuff" to sudo on a remote host
 (eval-after-load 'tramp
