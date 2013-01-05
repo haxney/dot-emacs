a82147a9e0a7b7bd56a4c5267adb789e5ff4d852
Daniel Hackney <dan@haxney.org>
2013-01-04 19:26:44 -0500

    Make `magit-status' take up the whole frame

--- load.d/50keybindings.el
+++ load.d/50keybindings.el
@@ -98,3 +98,18 @@
   (magit-refresh))
 
 (define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
+
+;; full screen magit-status
+
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
