ce8446ded36b09383951f5803ff515da4a159a74
Daniel Hackney <dan@haxney.org>
2013-01-04 19:24:53 -0500

    Add function `magit-ignore-whitespace'

--- load.d/50keybindings.el
+++ load.d/50keybindings.el
@@ -80,3 +80,21 @@
 (eval-after-load 'geiser-mode
   '(progn
      (define-key geiser-mode-map [remap geiser-edit-symbol-at-point] 'helm-semantic-or-imenu)))
+
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
