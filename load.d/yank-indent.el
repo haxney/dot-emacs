d1cf482cb501c2b5ffd01ccd57c36be9736e2cb2
Daniel Hackney <dhackney@cs.brown.edu>
2013-02-27 23:35:33 -0500

    Add `yank-indent'.

---
+++ load.d/yank-indent.el
@@ -0,0 +1,60 @@
+;;; yank-indent.el --- Automatically indenting yanked text if in programming-modes
+
+
+;; Copyright (C) 2013 magnars
+
+;; This file is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation; either version 3, or (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful, but WITHOUT ANY
+;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+;; A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; GNU Emacs; see the file COPYING.  If not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+;; USA.
+
+;; This file is NOT part of GNU Emacs.
+
+;;; Commentary:
+
+;; From https://github.com/magnars/.emacs.d/blob/master/defuns/editing-defuns.el
+
+;;; Code:
+
+(defcustom yank-indent-modes '(prog-mode
+                               sgml-mode
+                               js2-mode)
+  "Modes in which to indent regions that are yanked (or yank-popped)")
+
+(defvar yank-advised-indent-threshold 1000
+  "Threshold (# chars) over which indentation does not automatically occur.")
+
+(defun yank-advised-indent-function (beg end)
+  "Do indentation, as long as the region isn't too large."
+  (if (<= (- end beg) yank-advised-indent-threshold)
+      (indent-region beg end nil)))
+
+(defadvice yank (after yank-indent activate)
+  "If current mode is one of 'yank-indent-modes, indent yanked text (with prefix arg don't indent)."
+  (if (and (not (ad-get-arg 0))
+           (apply 'derived-mode-p yank-indent-modes))
+      (let ((transient-mark-mode nil))
+        (yank-advised-indent-function (region-beginning) (region-end)))))
+
+(defadvice yank-pop (after yank-pop-indent activate)
+  "If current mode is one of 'yank-indent-modes, indent yanked text (with prefix arg don't indent)."
+  (if (and (not (ad-get-arg 0))
+           (apply 'derived-mode-p yank-indent-modes))
+      (let ((transient-mark-mode nil))
+        (yank-advised-indent-function (region-beginning) (region-end)))))
+
+(defun yank-unindented ()
+  (interactive)
+  (yank 1))
+
+(provide 'yank-indent)
+
+;;; yank-indent.el ends here
