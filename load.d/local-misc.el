62cb06b239031a3d1c00ca0f8987bba9f9003b41
Daniel Hackney <dan@haxney.org>
2013-05-11 14:28:05 -0400

    Enable paredit mode in the minibuffer during `eval-expression'

--- load.d/local-misc.el
+++ load.d/local-misc.el
@@ -55,4 +55,11 @@
 
 (global-set-key (kbd "C-x Q") 'simple-macro-query)
 
+(defun conditionally-enable-paredit-mode ()
+  "Enable Paredit mode in the minibuffer during `eval-expression'"
+  (if (eq this-command 'eval-expression)
+      (paredit-mode 1)))
+
+(add-hook 'minibuffer-setup-hook 'conditionally-enable-paredit-mode)
+
 ;;; local-misc.el ends here
