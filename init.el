f1f79fd798c2947242865b2ee651ec02a6a5277f
Daniel Hackney <dan@haxney.org>
2021-03-23 21:31:15 -0500

    Don't use stable MELPA
    It doesn't have many of the packages I need.

--- init.el
+++ init.el
@@ -21,7 +21,7 @@
 
 (require 'package)
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("melpa" . "https://stable.melpa.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")))
 
 (package-initialize)
